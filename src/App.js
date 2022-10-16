bb97f25485d0689add05f5399d201aa241696c49
JackCaio <caio.iack1@gmail.com>
2022-10-16 20:47:20 -0300

    feat: Adicao de principais rotas

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import LandingPage from './pages/LandingPage';
 import Legacy from './pages/Legacy';
+import Projects from './pages/Projects';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={ LandingPage } />
+        <Route path="/projects" component={ Projects } />
+        <Route path="/contact" component={ Projects } />
         <Route path="/legacy" component={ Legacy } />
       </Switch>
     </BrowserRouter>
---
+++ src/pages/Projects.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Template from '../components/Template';
+
+export default class Projects extends Component {
+  render() {
+    return (
+      <Template>
+        <h1>Projects</h1>
+      </Template>
+    );
+  }
+}
