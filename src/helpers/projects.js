b2ce5d901377217512f7181be23268d22d7bbcf1
JackCaio <caio.iack1@gmail.com>
2023-01-27 18:58:22 -0300

    feat: Adiciona dados de projeto publicado

---
+++ src/helpers/projects.js
@@ -0,0 +1,11 @@
+/* eslint-disable max-len */
+const projects = [
+  {
+    name: 'React Trivia',
+    demo: 'https://jackcaio.github.io/ReactTrivia',
+    gith: 'https://github.com/JackCaio/ReactTrivia',
+    desc: 'Projeto em grupo, para desenvolvimento de jogo de Trivia com uso da API `Open Trivia DB, salvando leaderboard local`',
+  },
+];
+
+export default projects;
