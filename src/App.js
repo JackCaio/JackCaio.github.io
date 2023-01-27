d9c2493e09933d8cb782938b8356c92f9638b668
JackCaio <caio.iack1@gmail.com>
2023-01-27 19:00:51 -0300

    chores: Remove linha antiga comentada

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { Provider } from 'react-redux';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import LandingPage from './pages/LandingPage';
-// import Legacy from './pages/Legacy';
 import Projects from './pages/Projects';
 import store from './redux';
 
